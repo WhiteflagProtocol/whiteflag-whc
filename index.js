6b118885a0221f37ff53f359958b6c1b4c08ea22
ts5746 <timo@whiteflagprotocol.org>
2019-10-20 13:10:18 +0200

    Initial command line arguments

--- index.js
+++ index.js
@@ -1,10 +1,48 @@
 #!/usr/bin/env node
 'use strict';
-
- /**
+/**
  * @module index
  * @summary Whiteflag WHL main module
  * @description Module called first when invoking the Whiteflag WHL utility
  */
-console.log('The whiteflag-whl cli is in development');
+
+// Whiteflag WHL modules //
+const args = require('./lib/arguments');
+
+/*
+ * Gracefully crash if an uncaught exception occurs and
+ * ensure proper shutdonwn when process is stopped
+ */
+process.on('uncaughtException', uncaughtExceptionCb);
+process.on('SIGINT', shutdownCb);
+process.on('SIGTERM', shutdownCb);
+
+// MAIN PROCESS FLOW //
+let options = args.parse(process.argv);
+console.log(`Retrieving site: ${options.site}`);
 process.exit(0);
+
+// PRIVATE FUNCTIONS //
+/**
+ * Callback to log uncaught exceptions
+ * @callback shutdownCb
+ * @param {object} err error object if any error
+ */
+function shutdownCb() {
+    console.log('Caught SIGINT or SIGTERM. Exiting.');
+    return process.exit(0);
+}
+
+/**
+ * Callback to log uncaught exceptions
+ * @callback uncaughtExceptionCb
+ * @param {object} err error object if any error
+ */
+function uncaughtExceptionCb(err) {
+    if (err.stack) {
+        console.log(`UNCAUGHT EXCEPTION: ${err.stack}`);
+    } else {
+        console.log(`UNCAUGHT EXCEPTION: ${err.toString()}`);
+    }
+    return process.exit(2);
+}
