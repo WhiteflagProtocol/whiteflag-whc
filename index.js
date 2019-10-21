9d05b7c5f6bb87c829973e67a25e3a5fcb648eae
ts5746 <timo@whiteflagprotocol.org>
2019-10-21 17:24:37 +0200

    Whiteflag message creation

--- index.js
+++ index.js
@@ -9,35 +9,43 @@
 // Whiteflag WHL modules //
 const args = require('./lib/arguments');
 const sites = require('./lib/sites');
+const whiteflag = require('./lib/whiteflag');
 
 /*
  * Gracefully crash if an uncaught exception occurs and
  * ensure proper shutdonwn when process is stopped
  */
 process.on('uncaughtException', uncaughtExceptionCb);
-process.on('SIGINT', shutdownCb);
-process.on('SIGTERM', shutdownCb);
+process.on('SIGINT', interruptCb);
+process.on('SIGTERM', interruptCb);
 
-// MAIN PROCESS FLOW //
-args.parse(process.argv, function argumentsCb(err, options) {
+// MAIN //
+main(function mainCb(err) {
     if (err) errorHandler(err);
-
-    // Get the requested site
-    sites.get(options, function sitesCb(err, site) {
-        if (err) errorHandler(err);
-        process.exit(0);
-    });
+    process.exit(0);
 });
 
 // PRIVATE FUNCTIONS //
 /**
- * Callback to log uncaught exceptions
- * @callback shutdownCb
- * @param {object} err error object if any error
+ * Main process function
+ * @function main
+ * @param {function} callback
  */
-function shutdownCb() {
-    console.log('Caught SIGINT or SIGTERM. Exiting.');
-    return process.exit(0);
+function main(callback) {
+    args.parse(process.argv, function argumentsCb(err, options) {
+        if (err) return callback(err);
+
+        // Get the requested site
+        sites.get(options, function sitesCb(err, site) {
+            if (err) return callback(err);
+
+            // Process Whiteflag messages
+            whiteflag.processData(site, options, function wfProcessDataCb(err) {
+                if (err) return callback(err);
+                return callback(null);
+            });
+        });
+    });
 }
 
 /**
@@ -47,7 +55,17 @@ function shutdownCb() {
  */
 function errorHandler(err) {
     console.error(err.message);
-    return process.exit(1);
+    return process.exit(2);
+}
+
+/**
+ * Callback to log uncaught exceptions
+ * @callback interruptCb
+ * @param {object} err error object if any error
+ */
+function interruptCb() {
+    console.log('Caught SIGINT or SIGTERM. Exiting.');
+    return process.exit(130);
 }
 
 /**
