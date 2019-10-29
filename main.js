276e252aa0a0d1bb4238993a13ba63524bd1980d
Timo <35366924+ts5746@users.noreply.github.com>
2019-10-29 14:39:10 +0100

    Version 0.2.0

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 /**
- * @module index
+ * @module main
  * @summary Whiteflag WHL main module
  * @description Module called first when invoking the Whiteflag WHL utility
  */
 
+// Node.js core and external modules //
+const yargs = require('yargs');
+
 // Whiteflag WHL modules //
-const args = require('./lib/arguments');
-const sites = require('./lib/sites');
+const whl = require('./lib/whl');
 const whiteflag = require('./lib/whiteflag');
 
 /*
@@ -32,17 +34,21 @@ main(function mainCb(err, exitcode = 0) {
  * @param {function} callback
  */
 function main(callback) {
-    args.parse(process.argv, function argumentsCb(err, options) {
-        if (err) return callback(err);
+    // Parse command line options
+    const options = parseArguments(process.argv);
 
-        // Get the requested site
-        sites.get(options, function sitesCb(err, sites) {
-            if (err) return callback(err, 2);
-            if (sites.length === 0) return callback(null, 1);
+    // Get the requested WHL site data
+    whl.getSites(options, function whlGetSitesCb(err, sites) {
+        if (err) return callback(err, 2);
+        if (sites.length === 0) return callback(null, 1);
 
-            // Process Whiteflag messages
-            whiteflag.processData(sites, options, function wfProcessDataCb(err) {
-                if (err) return callback(err);
+        // Create Whiteflag messages from WHL site data
+        whiteflag.createMessages(sites, options, function wfCreateMessagesCb(err, wfMessages) {
+            if (err) return callback(err);
+
+            // Send the Whiteflag messages
+            whiteflag.sendMessages(wfMessages, options, function wfSendMessagesCb(err) {
+                if (err) return callback(err, 2);
                 return callback(null, 0);
             });
         });
@@ -82,4 +88,72 @@ function uncaughtExceptionCb(err) {
         console.error(`UNCAUGHT EXCEPTION: ${err.toString()}`);
     }
     return process.exit(2);
+}
+
+/**
+ * Parses the provided command line arguments
+ * @private
+ * @param {array} argv array with arguments passed to process
+ * @returns {object} parsed options provided on the command line
+ */
+function parseArguments(argv) {
+    // Define arguments
+    const args = yargs
+    .usage('Usage: $0 [-s <id> ...] [-w <url>|-f <file>]\n             [-t -i <url> -b <blockchain> -a <address>] [-ov]')
+    .config()
+    .option('s', {
+        alias: 'sites',
+        describe: 'Specify the world heritage site(s) by <id> number',
+        type: 'array'
+    })
+    .option('w', {
+        alias: 'web',
+        describe: 'The <url> of the WHL in XML on the web',
+        type: 'string',
+        requiresArg: true,
+        conflicts: 'f'
+    })
+    .option('f', {
+        alias: 'file',
+        describe: 'The <file> containing the WHL in XML',
+        type: 'string',
+        requiresArg: true,
+        normalize: true,
+        conflicts: 'w'
+    })
+    .option('t', {
+        alias: 'transmit',
+        describe: 'Transmit the Whiteflag message(s)',
+        type: 'boolean',
+        implies: ['i', 'b', 'a']
+    })
+    .option('i', {
+        alias: 'interface',
+        describe: 'The Whiteflag API REST interface <url>',
+        type: 'string',
+        requiresArg: true
+    })
+    .option('b', {
+        alias: 'blockchain',
+        describe: 'The <blockhain> to be used',
+        type: 'string',
+        requiresArg: true
+    })
+    .option('a', {
+        alias: 'address',
+        describe: 'The blockchain <address> to be used',
+        type: 'string',
+        requiresArg: true
+    })
+    .option('o', {
+        alias: 'stdout',
+        describe: 'Send the Whiteflag message(s) to stdout',
+        type: 'boolean'
+    })
+    .option('v', {
+        alias: 'verbose',
+        describe: 'Provide detailed processing output',
+        type: 'boolean'
+    });
+    return args.parse(argv);
 }
